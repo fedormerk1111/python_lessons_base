4415dc082cd7c468aa1624129a9981ed61562430
Fedor Merkulov <merkulov-f@inbox.ru>
2023-10-21 18:53:42 +0300

    урок 1: добавлено решение 1-й задачи и pass на вторую

--- lesson01/homework01.py
+++ lesson01/homework01.py
@@ -3,4 +3,13 @@
 несколько чисел и строк и сохраните в переменные, выведите на экран.
 """
 
+string_value = 'Делааа...'
+int_value = 21
 
+print(string_value, int_value)
+
+user_string_value = input("Введите строку: ")
+user_int_value_1 = int(input("Введите число: "))
+user_int_value_2 = int(input("Введите число: "))
+
+print(f"Была введена строка '{user_string_value}', первое число: {user_int_value_1}, второе число: {user_int_value_2}")
