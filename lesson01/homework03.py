a76a63c98c6129bdb12e71382ef08112d0910ba0
Fedor Merkulov <merkulov-f@inbox.ru>
2023-10-21 20:30:23 +0300

    урок 1: добавлено решение задач 1, 2, 3 и 4.

--- lesson01/homework03.py
+++ lesson01/homework03.py
@@ -3,4 +3,16 @@
 ввёл число 3. Считаем 3 + 33 + 333 = 369.
 """
 
+'''
+user_num = input("Введи число: ")       # Решение через сложение строк и преобразование сложенных строк в числа
 
+num_1 = int(user_num)
+num_2 = int(user_num + user_num)
+num_3 = int(user_num + user_num + user_num)
+
+print(f"{num_1} + {num_2} + {num_3} = {num_1 + num_2 + num_3}")
+'''
+
+user_num = int(input("Введи число: "))      # Решение через умножение
+
+print(f"{user_num} + {user_num * 11} + {user_num * 111} = {user_num * 123}")
--- lesson01/homework04.py
+++ lesson01/homework04.py
@@ -2,3 +2,15 @@
 Пользователь вводит целое положительное число. Найдите самую большую цифру в числе.
 Для решения используйте цикл while и арифметические операции.
 """
+
+user_num = list(input("Введи число: "))
+
+index = 1
+result = user_num[0]
+
+while index < len(user_num):
+    if user_num[index] > result:
+        result = user_num[index]
+    index += 1
+
+print(result)
